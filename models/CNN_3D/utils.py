8ae4ec4b8e9aba6dfe0a2368399c92c59d650eca
jared-slone <51179852+jared-slone@users.noreply.github.com>
2024-01-09 12:15:58 -0600

    clean_house

--- models/CNN_3D/utils.py
+++
@@ -1,4 +0,0 @@
-cn = {'P':0,'M':1,'A':2,'B':3}
-radii = {'ALA':3.20,'LEU':4.24,'ARG':5.60,'LYS':5.02,'ASN':4.04,'MET':4.47,'ASP':4.04,'PHE':4.99,'CYS':3.65,'PRO':3.61,'GLN':4.64,'SER':3.39,'GLU':4.63,'THR':3.56,'GLY':1.72,'TRP':5.38,'HIS':4.73,'TYR':5.36,'ILE':3.94,'VAL':3.55}
-# radius from https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7062742/
-AA_1hot_idx = {'ALA':0,'LEU':1,'LYS':2,'ASN':3,'MET':4,'ASP':5,'PHE':6,'CYS':7,'PRO':8,'GLN':9,'SER':10,'GLU':11,'THR':12,'GLY':13,'TRP':14,'HIS':15,'TYR':16,'ILE':17,'VAL':18,'ARG':19}    
